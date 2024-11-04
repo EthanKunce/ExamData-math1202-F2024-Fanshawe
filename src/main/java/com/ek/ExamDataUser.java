f477bdcfecd8170394bd93601d242bd561fa2c11
Ethan Kunce <ethankunce@ymail.com>
2024-11-04 18:35:20 -0500

    rewrote drawGraph() into 2 seperate methods, barGraphScores() shows 6 different graphs, drawWindow invoves the window.

--- src/main/java/com/ek/ExamDataUser.java
+++ src/main/java/com/ek/ExamDataUser.java
@@ -5,12 +5,15 @@
  * Desc:    A main program with helper methods to input the exams.csv as an array of ExamData class.  
  */
 package com.ek;
+
 //Dependencies for File I/O/
 import java.io.File;
 import java.util.Scanner;
 
 //Dependencies for drawing
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -20,38 +23,38 @@ import javax.swing.JPanel;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.category.CategoryDataset;
 import org.jfree.data.category.DefaultCategoryDataset;
-import org.jfree.data.xy.XYSeries;
-import org.jfree.data.xy.XYSeriesCollection;
 
+import com.ek.ExamData.ParentalEducation;
 import com.ek.ExamData.Races;
 
 public class ExamDataUser {
-    public static void main(String[] args) throws Exception{
+    // The tokenizeFile contains file I/O manipulation and may throw an Exception, if main calls it then main must also throw Exception
+    public static void main(String[] args) throws Exception {
         String PATH = "C:\\Users\\Ethan\\Documents\\VsCode\\examsPlay\\ExamAnalysis\\resources\\exams.csv";
         ExamData arr[] = tokenizeFile(PATH);
 
-        for(int i = 0; i < arr.length; i++){
+        for (int i = 0; i < arr.length; i++) {
             System.out.println("i = " + i + ": " + arr[i].toString());
-        }        
+        }
 
-        drawGraph(arr);
-        
+        drawWindow(arr);
+
     }
 
-    public static void drawGraph(ExamData arr[]){
-        
+    // Draw window is passed the Exam Data arr[] to perform analysis connections. 
+    // tokenizeFile is not called in here becuase we want to isolate exceptions thrown.
+    public static void drawWindow(ExamData arr[]) {
 
-        
-
-
-        //Create and configure the window
+        // Create and configure the window
         JFrame window = new JFrame();
         window.setTitle("Graph1");
         window.setSize(600, 400);
         window.setLayout(new BorderLayout());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+        // groupsMathScores is a method call that returns a ChartPanel object that can be added to our window. 
+        // Note passing an entire array may be bad for memory.  The ExamData array is passed.
 
         // Let's create a dropdown box
         JComboBox<String> firstDropDown = new JComboBox<String>();
@@ -67,42 +70,118 @@ public class ExamDataUser {
         firstDropDown.addItem("Test2");
         firstDropDown.addItem("Test3");
 
-        // Create a line graph
-        XYSeries series = new XYSeries("Math Scores");
-        XYSeriesCollection dataset = new XYSeriesCollection(series);
+        // int scoreIndex = 0;
+        // int enumIndex = 0;
 
-        DefaultCategoryDataset examdataRaces = new DefaultCategoryDataset();
-        // for(ExamData student : arr)
+        firstDropDown.addActionListener(new ActionListener() {
+            @Override public void actionPerformed(ActionEvent arg0){
+                int scoreIndex = firstDropDown.getSelectedIndex();
+                
+                // window.remove(0);
+                window.add(barGraphScores(arr, scoreIndex, 0), BorderLayout.CENTER);
+                window.transferFocus();
+                window.repaint();
+            } 
+        });
 
-        // Calculate the avg of each group;
-        int groupCounts[] = {0, 0, 0, 0, 0};
-        double groupAvg[] = new double[5];
-
-        for(ExamData student : arr){
-            groupAvg[student.group.ordinal()] += student.mathScore;
-            groupCounts[student.group.ordinal()]++;
-        }
-
-        for(int i = 0; i < groupAvg.length; i++){
-            groupAvg[i] /= groupCounts[i];
-            // This sets the value for each bar
-            examdataRaces.setValue(groupAvg[i], "Ethnic Groups", Races.values()[i]);
-        }
-        // dataset.addSeries(series);
-        JFreeChart chart = ChartFactory.createBarChart("Ethnicity and Math Scores", "Ethnic Groups", "Math Scores", examdataRaces);
-        window.add(new ChartPanel(chart), BorderLayout.CENTER);
+        // window.add(barGraphScores(arr, 1, 0), BorderLayout.CENTER);
 
 
         // Show the Window (Keep this at the end)
         window.setVisible(true);
     }
 
-    public static ExamData [] tokenizeFile(String Path) throws Exception {
+    // returns a Chart Panel object containing a bar graph titled Ethnicity and Math Scores
+    // It also calculates the the averages for each group
+    public static ChartPanel groupsMathScores(ExamData arr[]) {
+        DefaultCategoryDataset examdataRaces = new DefaultCategoryDataset();
+        // for(ExamData student : arr)
+        int numGroups = Races.values().length;
+        // Calculate the avg of each group;
+        int groupCounts[] = new int[numGroups];
+        double groupAvg[] = new double[numGroups];
+
+        for (ExamData student : arr) {
+            // based on the enumeated value of the students group, add their Math score with their groups
+            groupAvg[student.group.ordinal()] += student.mathScore;
+            // based on the enumeated value of the students group, increment their number of students
+            groupCounts[student.group.ordinal()]++;
+        }
+
+        for (int i = 0; i < numGroups; i++) {
+            groupAvg[i] /= groupCounts[i];
+            // This line prevents our default empty enum GroupNULL from being printed
+            if(groupAvg[i] != 0 && groupCounts[i] != 0)
+                examdataRaces.setValue(groupAvg[i], "Ethnic Groups", Races.values()[i]); // This sets the value for each bar
+        }
+        // dataset.addSeries(series);
+        JFreeChart chart = ChartFactory.createBarChart("Ethnicity and Math Scores", "Ethnic Groups", "Math Scores", examdataRaces);
+        // window.add(new ChartPanel(chart), BorderLayout.CENTER);
+
+        return new ChartPanel(chart);
+
+    }
+
+
+    public static ChartPanel barGraphScores(ExamData arr[], int scoreIndex, int enumIndex) {
+        DefaultCategoryDataset examdataRaces = new DefaultCategoryDataset();
+        // for(ExamData student : arr)
+        int numGroups = enumIndex == 0 ? Races.values().length : ParentalEducation.values().length;
+        String categoryAxis = enumIndex == 0 ? "Ethnic Groups" : "Parental Education";
+        String valueAxis = "";
+        // Calculate the avg of each group;
+        int groupCounts[] = new int[numGroups];
+        double groupAvg[] = new double[numGroups];
+
+        for (ExamData student : arr) {
+            switch (scoreIndex) {
+                case 0:             //Math Case
+                    // based on the enumeated value of the students group, add their Math score with their groups
+                    groupAvg[student.group.ordinal()] += student.mathScore;
+                    valueAxis = "Math Scores";
+                    break;
+                case 1:             //writing Case
+                    // based on the enumeated value of the students group, add their Math score with their groups
+                    groupAvg[student.group.ordinal()] += student.writingScore;
+                    valueAxis = "Writing Scores";
+                    break;
+                case 2:             //reading Case
+                    // based on the enumeated value of the students group, add their Math score with their groups
+                    groupAvg[student.group.ordinal()] += student.readingScore;
+                    valueAxis = "Reading Scores";
+                    break;
+                default:
+                    break;
+            }
+            // based on the enumeated value of the students group, increment their number of students
+            groupCounts[student.group.ordinal()]++;
+        }
+
+        for (int i = 0; i < numGroups; i++) {
+            groupAvg[i] /= groupCounts[i];
+            // This line prevents our default empty enum GroupNULL from being printed
+            if(groupAvg[i] != 0 && groupCounts[i] != 0)
+                examdataRaces.setValue(groupAvg[i], categoryAxis, enumIndex == 0 ? Races.values()[i] : ParentalEducation.values()[i]); // This sets the value for each bar
+        }
+
+        String chartTitle = categoryAxis + " and " + valueAxis;
+        // dataset.addSeries(series);
+        JFreeChart chart = ChartFactory.createBarChart(chartTitle, categoryAxis, valueAxis, examdataRaces);
+        // window.add(new ChartPanel(chart), BorderLayout.CENTER);
+
+        return new ChartPanel(chart);
+
+    }
+
+    public static ExamData[] tokenizeFile(String Path) throws Exception {
         Scanner fileInput = new Scanner(new File(Path));
         int index = 0;
         // .useDelimiter("[\"]+");
         // StringBuilder wholeFile = new StringBuilder();
         // true if lunch is reduced/Free, false otherwise
+
+        // ExamData does not have any overidden constructors so its easiest to just
+        // create a copy of an ExamData Data and call the contructor with all parameters
         Boolean tlunchReduced;
         Boolean ttestPrepCourse;
         Boolean tgender;
@@ -112,20 +191,19 @@ public class ExamDataUser {
         int treadingScore;
         int twritingScore;
 
-        ExamData store [] = new ExamData[100];
+        ExamData store[] = new ExamData[100];
 
-        while(fileInput.hasNext())
-        {
+        while (fileInput.hasNext()) {
             // System.out.println(fileInput.nextLine());
-            String []temp = fileInput.nextLine().split("[\",]+");
+            String[] temp = fileInput.nextLine().split("[\",]+");
 
             // for (int i = 0; i < temp.length; i++) {
-            //     System.out.println("i = " + i + ": " + temp[i]);
+            // System.out.println("i = " + i + ": " + temp[i]);
             // }
 
             tgender = temp[1].equals("male");
-        
-            switch(temp[2]){
+
+            switch (temp[2]) {
                 case "group A":
                     tgroup = ExamData.Races.GroupA;
                     break;
@@ -137,7 +215,7 @@ public class ExamDataUser {
                     break;
                 case "group D":
                     tgroup = ExamData.Races.GroupD;
-                    break;                    
+                    break;
                 case "group E":
                     tgroup = ExamData.Races.GroupE;
                     break;
@@ -145,7 +223,7 @@ public class ExamDataUser {
                     tgroup = ExamData.Races.GroupNull;
                     break;
             }
-            switch(temp[3]){
+            switch (temp[3]) {
                 case "some high school":
                     tpe = ExamData.ParentalEducation.SHigh;
                     break;
@@ -163,27 +241,25 @@ public class ExamDataUser {
                     break;
                 case "master's degree":
                     tpe = ExamData.ParentalEducation.MDegree;
-                    break;             
+                    break;
                 default:
                     tpe = ExamData.ParentalEducation.PENull;
                     break;
-            }            
+            }
             tlunchReduced = !temp[4].equals("standard");
             ttestPrepCourse = temp[5].equals("completed");
 
             tmathScore = Integer.parseInt(temp[6]);
             twritingScore = Integer.parseInt(temp[7]);
             treadingScore = Integer.parseInt(temp[8]);
-                     
+
             store[index] = new ExamData(tlunchReduced, ttestPrepCourse, tgender, tgroup, tpe, tmathScore, treadingScore, twritingScore);
             index++;
-            // System.out.println(index);   
-        }        
-        
+            // System.out.println(index);
+        }
 
         fileInput.close();
         return store;
     }
-
 
 }
